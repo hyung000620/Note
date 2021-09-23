110b00745dd2d4e76183e6a1f73f1a11f958a10f
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-09-23 12:12:06 +0900

    Create 다리를 지나는 트럭.java

---
+++ Coding-Test/LEVEL2/다리를 지나는 트럭.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<int[]> queue = new LinkedList<>();
+        int time = 0, idx = 0;
+
+        while (idx < truck_weights.length) {
+            if (!queue.isEmpty() && time == queue.peek()[1]) {
+                int[] truck = queue.poll();
+                weight += truck[0];
+            }
+
+            if (weight >= truck_weights[idx]) {
+                queue.add(new int[]{truck_weights[idx], time + bridge_length});
+                weight -= truck_weights[idx++];
+            }
+
+            time++;
+        }
+
+        return time + bridge_length;
+    }
+}
