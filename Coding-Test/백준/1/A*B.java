61dc61691a3a1e057dd8a71550bff3b41645376b
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-09-30 09:54:38 +0900

    Create A*B.java

---
+++ Coding-Test/백준/1/A*B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int A, B, result;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        A = scan.nextInt();
+        B = scan.nextInt();
+        
+        result = A*B;
+        
+        System.out.println(result);
+    }
+}
