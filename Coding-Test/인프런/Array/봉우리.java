5ba8c1e64d9c0dde84a7a46a4ad1b04878e24451
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-10-23 10:26:51 +0900

    Create 봉우리.java

---
+++ Coding-Test/인프런/Array/봉우리.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main {
+  int[] dx = {-1,0,1,0};
+  int[] dy = {0,1,0,-1};
+  public int solution(int n, int[][] arr){
+    int answer= 0;
+    for(int i=0; i<n; i++){
+    	for(int j=0; j<n; j++){
+        	boolean flag = true;
+          	for(int k=0; k<4; k++){
+              int nx = i+dx[k];
+              int ny = j+dy[k];
+            if(nx>=0 && nx<n && ny>=0 && ny<n && arr[nx][ny]>=arr[i][j]){
+            	flag= false;
+              	break;
+            }
+            }
+          if(flag) answer ++;
+        }
+    }
+    return answer;
+  }
+  public static void main(String[] args){
+	Main m = new Main();
+    Scanner in=new Scanner(System.in);
+    int n = in.nextInt();
+    int[][] arr = new int[n][n];
+    for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+        arr[i][j] = in.nextInt();
+      }
+    }
+    System.out.println(m.solution(n, arr));
+  }
+}
