da89787f4115eef907c8924ec82cfaf2f12ffb0a
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-10-25 16:34:46 +0900

    Create 크레인 인형뽑기.java

---
+++ Coding-Test/인프런/Stack, Queue(자료구조)/크레인 인형뽑기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+  
+public class Main {
+  public int solution(int n, int[][] arr, int move, int[] board){
+    int answer =0;
+    Stack<Integer> stack = new Stack<>();
+    for(int pos : board){
+    	for(int i =0; i<n; i++){
+        	if(arr[i][pos-1]!=0){
+              int tmp = arr[i][pos-1];
+              arr[i][pos-1] = 0;
+              if(!stack.isEmpty() && tmp == stack.peek()){answer+=2; stack.pop();}
+              else{stack.push(tmp);}
+              break;
+            }
+    	}
+    }
+    
+    return answer;
+  }
+  public static void main(String[] args){
+    Main T = new Main();
+    Scanner in=new Scanner(System.in);
+    int n = in.nextInt();
+    int[][] arr = new int[n][n];
+    for(int i=0; i<n; i++){
+    	for(int j=0; j<n; j++){
+        	arr[i][j] = in.nextInt();
+        }
+    }
+    int m = in.nextInt();
+    int[] board = new int[m];
+    for(int k=0; k<m; k++){
+    	board[k] = in.nextInt();
+    }
+    System.out.println(T.solution(n, arr, m, board));
+  }
+}
