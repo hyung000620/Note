7e6d4d26f1da22dd2c228b05b4fd0e75ebd2f6d5
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-10-27 13:44:52 +0900

    Create 삽입 정렬.java

---
+++ Coding-Test/인프런/정렬, 이분검색과 결정알고리즘/삽입 정렬.java
@@ -0,0 +1,27 @@
+import java.util.*;
+  
+public class Main {
+  public int[] solution(int n, int[] arr){
+  	for(int i=1; i<n; i++){
+      	int tmp = arr[i], j;
+    	for(j=i-1; j>=0; j--){
+        	if(arr[j]>tmp) arr[j+1] = arr[j];
+          	else break;
+        }
+      	arr[j+1] = tmp;
+    }
+    return arr;
+  }
+  public static void main(String[] args){
+    Main T = new Main();
+    Scanner in=new Scanner(System.in);
+    int n = in.nextInt();
+    int[] arr = new int[n];
+    for(int i=0; i<n; i++){
+    	arr[i] = in.nextInt();
+    }
+    for(int x : T.solution(n, arr)){
+    System.out.print(x+" ");
+  }
+  }
+}
