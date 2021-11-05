d3e9ecc4937c9a546b32dded9ffef538fccf40d6
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-11-05 12:38:49 +0900

    Create 바둑이 승차(DFS).java

---
+++ Coding-Test/인프런/DFS,BFS 활용/바둑이 승차(DFS).java
@@ -0,0 +1,27 @@
+import java.util.*;
+  
+public class Main {
+  static int answer = Integer.MIN_VALUE,c,n;
+  public void DFS(int L, int sum, int[] arr){
+  	if(sum>c) return;
+    if(L==n){
+      answer = Math.max(answer, sum);
+    }
+    else{
+    	DFS(L+1,sum+arr[L],arr);
+      	DFS(L+1,sum,arr);
+    }
+  }
+  public static void main(String[] args){
+    Main T = new Main();
+    Scanner in=new Scanner(System.in);
+    c = in.nextInt();
+    n = in.nextInt();
+    int[] arr = new int[n];
+    for(int i=0; i<n; i++){
+    	arr[i]= in.nextInt();
+    }
+    T.DFS(0,0,arr);
+    System.out.println(answer);
+  }
+}
