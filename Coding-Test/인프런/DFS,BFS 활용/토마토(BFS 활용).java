562aca2f8a2b547cfd071354da1f00d5425811ce
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-11-10 15:10:23 +0900

    Create 토마토(BFS 활용).java

---
+++ Coding-Test/인프런/DFS,BFS 활용/토마토(BFS 활용).java
@@ -0,0 +1,61 @@
+import java.util.*;
+class Point{
+	public int x, y;
+	Point(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+}
+class Main {
+	static int[] dx={-1, 0, 1, 0};
+	static int[] dy={0, 1, 0, -1};
+	static int[][] board, dis;
+	static int n, m;
+	static Queue<Point> Q=new LinkedList<>();
+	public void BFS(){
+		while(!Q.isEmpty()){
+			Point tmp=Q.poll();
+			for(int i=0; i<4; i++){
+				int nx=tmp.x+dx[i];
+				int ny=tmp.y+dy[i];
+				if(nx>=0 && nx<n && ny>=0 && ny<m && board[nx][ny]==0){
+					board[nx][ny]=1;
+					Q.offer(new Point(nx, ny));
+					dis[nx][ny]=dis[tmp.x][tmp.y]+1;
+				}
+			}
+		}	
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		m=kb.nextInt();
+		n=kb.nextInt();
+		board=new int[n][m];
+		dis=new int[n][m];
+		for(int i=0; i<n; i++){
+			for(int j=0; j<m; j++){
+				board[i][j]=kb.nextInt();
+				if(board[i][j]==1) Q.offer(new Point(i, j));
+			}
+		}
+		T.BFS();
+		boolean flag=true;
+		int answer=Integer.MIN_VALUE;
+		for(int i=0; i<n; i++){
+			for(int j=0; j<m; j++){
+				if(board[i][j]==0) flag=false;
+			}
+		}
+		if(flag){
+			for(int i=0; i<n; i++){
+				for(int j=0; j<m; j++){
+					answer=Math.max(answer, dis[i][j]);
+				}
+			}
+			System.out.println(answer);
+		}
+		else System.out.println(-1);
+	}
+}
