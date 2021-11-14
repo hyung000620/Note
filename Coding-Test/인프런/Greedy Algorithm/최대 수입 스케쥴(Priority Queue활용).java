d96620ec0edf8c55b4817223bc732612490e8c92
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-11-14 10:56:31 +0900

    Create 최대 수입 스케쥴(Priority Queue활용).java

---
+++ Coding-Test/인프런/Greedy Algorithm/최대 수입 스케쥴(Priority Queue활용).java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Lecture implements Comparable<Lecture>{
+    public int money;
+	public int time;
+    Lecture(int money, int time) {
+        this.money = money;
+        this.time = time;
+    }
+    @Override
+    public int compareTo(Lecture ob){
+        return ob.time-this.time;
+    }
+}
+
+class Main {
+	static int n, max=Integer.MIN_VALUE;
+	public int solution(ArrayList<Lecture> arr){
+		int answer=0;
+		PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+		Collections.sort(arr);
+		int j=0;
+		for(int i=max; i>=1; i--){
+			for(; j<n; j++){
+				if(arr.get(j).time<i) break;
+				pQ.offer(arr.get(j).money);
+			}
+			if(!pQ.isEmpty()) answer+=pQ.poll();
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		n=kb.nextInt();
+		ArrayList<Lecture> arr = new ArrayList<>();
+		for(int i=0; i<n; i++){
+			int m=kb.nextInt();
+			int d=kb.nextInt();
+			arr.add(new Lecture(m, d));
+			if(d>max) max=d;
+		}
+		System.out.println(T.solution(arr));
+	}
+}
