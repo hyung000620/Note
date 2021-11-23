15d0e1701bac05d467ff38901b8da43164bf7458
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-11-23 17:07:01 +0900

    Create 문제 1) 우리는 계단도 특별하게 오르죠.java

---
+++ Coding-Test/CosPro/5차/문제 1) 우리는 계단도 특별하게 오르죠.java
@@ -0,0 +1,31 @@
+class Main {
+    public int solution(int n) {
+        int answer = 0;
+
+        int[] steps = new int[n+1];
+        steps[1] = 1;
+        steps[2] = 2;
+        steps[3] = 4;
+        for(int i = 4; i <= n; i++)
+        	steps[i] = steps[i-1]+steps[i-2]+steps[i-3];
+        answer = steps[n];
+
+        return answer;
+    }
+
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+    	Main sol = new Main();
+    	int n1 = 3;
+    	int ret1 = sol.solution(n1);
+
+    	// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    	System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+
+    	int n2 = 4;
+    	int ret2 = sol.solution(n2);
+
+    	// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    	System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
+    }
+}
