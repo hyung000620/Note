03e54702740588f9ff83348a286258534cfaef58
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-11-25 14:39:20 +0900

    Update 문제 5) 소용돌이 수.java

--- Coding-Test/CosPro/1차/문제 5) 소용돌이 수.java
+++ Coding-Test/CosPro/1차/문제 5) 소용돌이 수.java
@@ -1,41 +1,41 @@
 // 다음과 같이 import를 사용할 수 있습니다.
 import java.util.*;
 class Main {
-		int[] dx = {0,1,0,-1};
+    int[] dx = {0,1,0,-1};
     int[] dy = {1,0,-1,0};
     public int solution(int n) {
-			int answer = 0;
-			int turn = (n%2) == 0? n/2 : (n/2)+1;
-			int cx, cy;
-			int cur = 1;
-			int[][] arr = new int[n][n];
-			for(int i=0; i<turn; i++){
-				int d = 0;
-				cx = i;
-				cy = i;
-				arr[cx][cy] = cur ++;
-				while(d<4){
-					int nx = cx + dx[d];
-					int ny = cy + dy[d];
-					if(nx >=n || nx<0 || ny>=n || ny<0 || arr[nx][ny]>0){
-						d++;
-						continue;
-					}
-					arr[nx][ny] = cur++;
-					cx = nx;
-					cy = ny;
-				}
-			}
-			answer = getResult(arr);
-			return answer;
-    }
-		public int getResult(int[][] arr){
-			int sum = 0;
-			for(int i=0; i<arr.length; i++){
-				sum += arr[i][i];
-			}
-			return sum;
+	int answer = 0;
+	int turn = (n%2) == 0? n/2 : (n/2)+1;
+	int cx, cy;
+	int cur = 1;
+	int[][] arr = new int[n][n];
+	for(int i=0; i<turn; i++){
+		int d = 0;
+		cx = i;
+		cy = i;
+		arr[cx][cy] = cur ++;
+		while(d<4){
+			int nx = cx + dx[d];
+			int ny = cy + dy[d];
+			if(nx >=n || nx<0 || ny>=n || ny<0 || arr[nx][ny]>0){
+				d++;
+				continue;
 		}
+				arr[nx][ny] = cur++;
+				cx = nx;
+				cy = ny;
+			}
+	}
+		answer = getResult(arr);
+		return answer;
+    }
+	public int getResult(int[][] arr){
+		int sum = 0;
+		for(int i=0; i<arr.length; i++){
+			sum += arr[i][i];
+		}
+		return sum;
+	}
   // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
