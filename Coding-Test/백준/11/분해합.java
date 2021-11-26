46c7b6a60790d727d7dd3fc59c594ed2586050f3
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-11-26 13:30:18 +0900

    Create 분해합.java

---
+++ Coding-Test/백준/11/분해합.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int answer = 0;
+        for(int i=0; i<N; i++){
+            int number = i;
+            int sum =0;
+            while(number!=0){
+                sum += number %10;
+                number /= 10;
+            }
+            if(sum +i == N){
+                answer = i;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}
