08119ac60cf50bb43032bfb27f1bd8aa88567872
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-04 10:19:55 +0900

    Update 아나그램(해쉬).java

--- Coding-Test/인프런/HashMap,TreeSet/아나그램(해쉬).java
+++ Coding-Test/인프런/HashMap,TreeSet/아나그램(해쉬).java
@@ -3,13 +3,13 @@ import java.util.*;
 public class Main {
   public String solution(String str1, String str2){
   	String answer ="YES";
-    HashMap<Character, Integer> map = new HashMap<>();
+    HashMap<Character, Integer> map = new HashMap<>(); // HashMap 의 Key: Character, Value: Integer
     for(char x : str1.toCharArray()){
-    	map.put(x,map.getOrDefault(x,0)+1);
+    	map.put(x,map.getOrDefault(x,0)+1); // Key가 하나라도 있으면 Value 값에 1이 입력된다.
     }
     for(char x : str2.toCharArray()){
-    	if(!map.containsKey(x) || map.get(x) == 0) return "NO";
-      	map.put(x,map.get(x)-1);
+    	if(!map.containsKey(x) || map.get(x) == 0) return "NO"; // Key 값이 존재하지 않거나 Key의 Value 값이 0일경우 아나그램이 되지 않는다.
+      	map.put(x,map.get(x)-1); //만약 Key값이 있을 경우 Value 값을 1을 빼준다.
     }
     return answer;
   }
