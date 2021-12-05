7c212072ac86350aea64ae29a4bce28fd661097a
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-05 09:59:23 +0900

    Update 모든 아나그램 찾기.java

--- Coding-Test/인프런/HashMap,TreeSet/모든 아나그램 찾기.java
+++ Coding-Test/인프런/HashMap,TreeSet/모든 아나그램 찾기.java
@@ -1,7 +1,7 @@
 import java.util.*;
   
-public class Main {
-  public int solution(String s, String t){
+public class Main { // Sliding Window 알고리즘 : 시간복잡도 O(n)
+  public int solution(String s, String t){ // ex) s : bacaAacba, t : abc
   	int answer =0;
     HashMap<Character, Integer> sMap = new HashMap<>();
     HashMap<Character, Integer> tMap = new HashMap<>();
@@ -11,18 +11,18 @@ public class Main {
     int lt=0;
     for(int rt=len; rt<s.length(); rt++){
     	sMap.put(s.charAt(rt),sMap.getOrDefault(s.charAt(rt),0)+1);
-        if(sMap.equals(tMap)) answer ++;
-        sMap.put(s.charAt(lt),sMap.get(s.charAt(lt))-1);
-        if(sMap.get(s.charAt(lt))==0) sMap.remove(s.charAt(lt));
-        lt++;         
+        if(sMap.equals(tMap)) answer ++; //Map과 Map의 equals는 Key 값과 Value 값이 동일하면 true를 리턴해준다.
+        sMap.put(s.charAt(lt),sMap.get(s.charAt(lt))-1); //밀고 나가야 되므로, 앞에 있는 Key의 Value 값을 1 감소시킨다.
+        if(sMap.get(s.charAt(lt))==0) sMap.remove(s.charAt(lt)); // 만약 앞에 있던 Key의 Value 값이 0이면 해당 Key값을 삭제시킨다.
+        lt++; //lt값 증가후 다시 for문이 돌면서 rt값이 증가.         
     }
     return answer;
   }
   public static void main(String[] args){
     Main M = new Main();
     Scanner in=new Scanner(System.in);
-  	String s = in.next();
-    String t = in.next();
+  	String s = in.next(); //ex) bacaAacba
+    String t = in.next(); //ex) abc
     System.out.println(M.solution(s,t));
   }
 }
