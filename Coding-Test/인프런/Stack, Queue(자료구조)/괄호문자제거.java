d16892688457e92247e35b63b69656d91b4f3a9a
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-07 09:01:03 +0900

    Update 괄호문자제거.java

--- Coding-Test/인프런/Stack, Queue(자료구조)/괄호문자제거.java
+++ Coding-Test/인프런/Stack, Queue(자료구조)/괄호문자제거.java
@@ -6,12 +6,15 @@ public class Main {
     Stack<Character> stack = new Stack<>();
     for(char x : str.toCharArray()){
       	if(x == ')'){
-        	while(stack.pop()!='(');
+        	while(stack.pop()!='('); // '('이 아닐때까지 Stack.pop()를 실행
         }else stack.push(x);
     }
     for(char x : stack){
     	answer += x;
     }
+    /**
+    for(int i=0; i<stack.size(); i++) answer += stack.get(i); //로도 가능
+    **/
     return answer;
   }
   public static void main(String[] args){
