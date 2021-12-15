3814d57915ca8a7a0d911591dd730c34ddd5989d
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-15 20:47:14 +0900

    Create 단어 정렬.java

---
+++ Coding-Test/백준/12/단어 정렬.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+        for(int i=0; i<N; i++) arr[i] = br.readLine();
+        
+        Arrays.sort(arr, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				// 단어 길이가 같을 경우 
+				if (s1.length() == s2.length()) {
+					return s1.compareTo(s2);
+				} 
+				// 그 외의 경우 
+				else {
+					return s1.length() - s2.length();
+				}
+			}
+		});
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(arr[0]).append('\n');
+        
+		for (int i = 1; i < N; i++) {
+			// 중복되지 않는 단어만 출력
+			if (!arr[i].equals(arr[i - 1])) {
+				sb.append(arr[i]).append('\n');
+			}
+		}
+        System.out.println(sb);
+    }
+}
