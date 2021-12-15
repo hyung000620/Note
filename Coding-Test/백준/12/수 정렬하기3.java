32d53903b5b4cf80a1b23987ae6749bd2c12d8e4
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-15 09:59:25 +0900

    Create 수 정렬하기3.java

---
+++ Coding-Test/백준/12/수 정렬하기3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++) arr[i]= Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(int x : arr){
+            sb.append(x).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
