6829f625daf7f0e5ef14fab086d469dd94d98371
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-25 11:21:28 +0900

    Create 오늘 날짜.java

---
+++ Coding-Test/백준/1/오늘 날짜.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Main{
+    public static void main(String[] args ){
+        LocalDate todaysDate = LocalDate.now();
+        System.out.println(todaysDate);
+    }
+}
