848e165c88d5474bbd0671a2a4f0ecb560f8d263
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-25 14:16:01 +0900

    Create 검증수.java

---
+++ Coding-Test/백준/1/검증수.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args ) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+		int ans = 0;
+		for(int i=0; i<5; i++){
+			int num = Integer.parseInt(st.nextToken());
+			ans += Math.pow(num, 2);
+		}
+
+		System.out.println(ans%10);
+    }
+}
