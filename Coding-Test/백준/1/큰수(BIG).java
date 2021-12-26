90b61de325fe65ae796f61a70711d525760fe44d
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-26 20:04:59 +0900

    Create 큰수(BIG).java

---
+++ Coding-Test/백준/1/큰수(BIG).java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        
+        long temp = 0;
+        for(int i=0; i<str.length(); i++){
+            temp = (temp*10+(str.charAt(i)-'0'))%20000303;
+        }
+        System.out.println(temp);
+    }
+}
