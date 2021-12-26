4fd4d084be7d899efad4c83a94cd40fa6d196712
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-26 11:43:50 +0900

    Create 세 수 정렬.java

---
+++ Coding-Test/백준/12/세 수 정렬.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+
+public class Main{
+
+    public static void main(String[] args ) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+		int[] arr = new int[3];
+		for(int i=0; i<3; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<3; i++){
+			sb.append(arr[i]).append(" ");
+		}
+		System.out.println(sb);
+	}
+}
