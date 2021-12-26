6cc3f525d988fc1ca07ada7cc14811737d1f0a0c
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-26 12:15:28 +0900

    Create 열 개씩 끊어 출력하기.java

---
+++ Coding-Test/백준/7/열 개씩 끊어 출력하기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<str.length(); i++){
+			sb.append(str.charAt(i));
+			if(i%10 == 9){
+				sb.append('\n');
+			}
+		}
+
+		System.out.println(sb);
+	}
+	
+}
