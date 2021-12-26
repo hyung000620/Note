81d8e6c48d8e61a90ab676da47559ea6f773f0d1
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-26 13:59:05 +0900

    Create 크냐?.java

---
+++ Coding-Test/백준/1/크냐?.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		while(true){
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			if(a == 0 && b ==0) break;
+
+			if(a<=b) sb.append("No").append('\n');
+			else sb.append("Yes").append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
