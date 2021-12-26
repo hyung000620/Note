6a038d6026c7d7647554525b8d1b4ad23c99fa83
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-26 13:28:32 +0900

    Create 긴자리 계산.java

---
+++ Coding-Test/백준/1/긴자리 계산.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		BigInteger A = new BigInteger(br.readLine());
+		BigInteger B = new BigInteger(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(A.add(B)).append('\n');
+		sb.append(A.subtract(B)).append('\n');
+		sb.append(A.multiply(B)).append('\n');
+
+		System.out.println(sb);
+	}
+	
+}
