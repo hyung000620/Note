59ab25de9c276395b5e35e3a3573bfdd1f6c1e43
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-26 18:48:53 +0900

    Create 스타후르츠.java

---
+++ Coding-Test/백준/1/스타후르츠.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+		int N = Integer.parseInt(st.nextToken())-1;
+		int T = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+
+		
+		System.out.println(P*((N/T)*C));
+    }
+}
