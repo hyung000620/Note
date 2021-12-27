f86ed4d1172da60e8dabf4cfe419406b3aaede35
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-27 16:04:30 +0900

    Create 10부제.java

---
+++ Coding-Test/백준/4/10부제.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int tmp = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+        int answer = 0;
+
+        for(int i=0; i<5; i++){
+            int preNum = Integer.parseInt(st.nextToken());
+            if(tmp == preNum){
+                answer ++;
+            }
+        }
+        
+        
+        System.out.println(answer);
+    }
+}
