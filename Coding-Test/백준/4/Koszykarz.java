c56769b5a92ab6f5e47cc549e8a3d98a699b3b73
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-27 21:07:16 +0900

    Create Koszykarz.java

---
+++ Coding-Test/백준/4/Koszykarz.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        Double k = Double.parseDouble(st.nextToken());
+        Double w = Double.parseDouble(st.nextToken());
+        Double m = Double.parseDouble(st.nextToken());
+
+        System.out.println((int)Math.ceil((w-k)/m));
+    }    
+}
