d7ff13b860903e7cc0fbafcd55cb79430f670779
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-30 17:26:04 +0900

    Create 별 찍기 - 9.java

---
+++ Coding-Test/백준/6/별 찍기 - 9.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=N-1; i>0; i--){
+            for(int j=1; j<N-i; j++){
+                sb.append(" ");
+            }
+            for(int j=0; j<(i+1)*2-1; j++){
+                sb.append("*");
+            }
+            sb.append('\n');
+        }
+        for(int i=0; i<N; i++){
+            for(int j=1; j<N-i; j++){
+                sb.append(" ");
+            }
+            for(int j=0; j<(i+1)*2-1; j++){
+                sb.append("*");
+            }
+            sb.append('\n');
+        }
+        
+        System.out.print(sb);
+    }
+}
