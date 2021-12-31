54be5b044be5a17a2548d876ce8232397099c8b7
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-31 16:35:51 +0900

    Create 삼각 무늬 - 1.java

---
+++ Coding-Test/백준/7/삼각 무늬 - 1.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+        int T = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(T-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            int tA = A*A;
+            int tB = B*B;
+
+            sb.append(tA/tB).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
