e38aa8019813f094cbfcd7ea129680a356f49f20
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-31 18:02:59 +0900

    Create 전체 계산 횟수.java

---
+++ Coding-Test/백준/7/전체 계산 횟수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int result = N;
+
+        while(N>=M){
+            N = N/M;
+            result += N;
+        }
+
+        bw.write(String.valueOf(result));
+
+        bw.flush();
+        bw.close();
+    }
+}
