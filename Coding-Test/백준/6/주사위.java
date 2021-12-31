f1c4d663192b9b66c2f97ce3d74d58dcedc1e090
JunHyung <81272766+hyung000620@users.noreply.github.com>
2021-12-31 14:56:13 +0900

    Create 주사위.java

---
+++ Coding-Test/백준/6/주사위.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append("Case ").append(i)
+            .append(": ").append(A+B).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
