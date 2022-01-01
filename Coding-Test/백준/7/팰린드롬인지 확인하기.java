1ff981d227596bbac71e12f91e2dfb6c959cdcf8
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-01-01 14:56:17 +0900

    Create 팰린드롬인지 확인하기.java

---
+++ Coding-Test/백준/7/팰린드롬인지 확인하기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+        String str2 = new StringBuilder(str).reverse().toString();
+                
+        if(str.equals(str2)){
+            bw.write(String.valueOf(1));
+        }else{
+            bw.write(String.valueOf(0));
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+}
