b8dd5734f30f5c81dfe486169c9f32393e8f20d9
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-01-02 21:40:17 +0900

    Create 돌 게임 2.java

---
+++ Coding-Test/백준/9/돌 게임 2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        String result = N%2==0?"SK":"CY";
+        bw.write(result);
+        bw.flush();
+        bw.close();
+    }
+}
