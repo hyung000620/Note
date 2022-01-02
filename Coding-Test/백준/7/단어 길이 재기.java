6f786bc8646971bb507c81bc65bfddf92b7454c6
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-01-02 15:50:25 +0900

    Create 단어 길이 재기.java

---
+++ Coding-Test/백준/7/단어 길이 재기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int result = br.readLine().length();
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+}
