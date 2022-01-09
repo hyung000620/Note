255d66bd33cc6e6a345f7afb05db38cab5115ad8
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-01-09 16:41:51 +0900

    Create Triangles.java

---
+++ Coding-Test/백준/10/Triangles.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            int n = Integer.parseInt(br.readLine());
+            if(n==0) break;
+
+            for(int i=0; i<n; i++){
+                for(int j=0; j<i+1; j++){
+                    sb.append('*');
+                }
+                sb.append('\n');
+            }
+        }
+        bw.write(sb.toString());
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    
+}
