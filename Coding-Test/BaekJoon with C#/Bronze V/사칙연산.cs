c22ed0ff43511873b1729b0f3cc2478ee2bcdfff
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-07-09 16:43:43 +0900

    Create 사칙연산.cs

---
+++ Coding-Test/BaekJoon with C#/Bronze V/사칙연산.cs
@@ -0,0 +1,6 @@
+string[] s = Console.ReadLine().Split();
+Console.WriteLine((int.Parse(s[0]) + int.Parse(s[1])).ToString());
+Console.WriteLine((int.Parse(s[0]) - int.Parse(s[1])).ToString());
+Console.WriteLine((int.Parse(s[0]) * int.Parse(s[1])).ToString());
+Console.WriteLine((int.Parse(s[0]) / int.Parse(s[1])).ToString());
+Console.WriteLine((int.Parse(s[0]) % int.Parse(s[1])).ToString());
