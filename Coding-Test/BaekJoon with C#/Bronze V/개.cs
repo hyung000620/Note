bb2b0ac07a416d2c12f6d62de7cf7f02462d0e1b
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-07-14 13:55:43 +0900

    Create 개.cs

---
+++ Coding-Test/BaekJoon with C#/Bronze V/개.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Prob10926
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine
+                (
+                "|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|"
+                );
+        }
+    }
+}
