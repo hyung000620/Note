34b33cc886380e52a2f96a20f4ed07474f97b84e
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-08-05 13:55:35 +0900

    Create 기찍 N.cs

---
+++ Coding-Test/BaekJoon with C#/Bronze IV/기찍 N.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using System.Text;
+using System;
+
+namespace codingtest
+{
+    class level3_baek{
+        public static void Main(){
+            
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            StringBuilder sb = new StringBuilder();
+            int N = int.Parse(sr.ReadLine());
+
+            for(int i = N; i >= 1; i--){
+                sb.Append(i);
+                sb.Append(System.Environment.NewLine);
+            }
+            sw.WriteLine(sb.ToString());
+            
+            sr.Close();
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
