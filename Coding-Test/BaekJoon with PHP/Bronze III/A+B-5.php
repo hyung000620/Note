a0fdcdf38a80ba471943300ed7b3f3a6a8673228
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-08-26 16:58:22 +0900

    Update A+B-5.php

--- Coding-Test/BaekJoon with PHP/Bronze III/A+B-5.php
+++ Coding-Test/BaekJoon with PHP/Bronze III/A+B-5.php
@@ -1,12 +1,7 @@
 <?php
 while (true) {
     fscanf(STDIN,"%d %d",$A,$B);
-    if ($A!=0 && $B!=0) {
-    $sum=$A+$B;
-    echo $sum."\n";
-    }
-    else {
-        break;
-    }
+    if ($A!=0 && $B!=0) {$sum=$A+$B;echo $sum."\n";}
+    else {break;}
 }
 ?>
