481be42dc3540a7512e82370a300a3096296d05f
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-09-11 13:46:54 +0900

    Update 최소,최대.php

--- Coding-Test/BaekJoon with PHP/Bronze III/최소,최대.php
+++ Coding-Test/BaekJoon with PHP/Bronze III/최소,최대.php
@@ -1,13 +1,14 @@
 <?php
-	$n = (int) fgets(STDIN);
-	$a = explode(' ', fgets(STDIN));
-	$inf = (int) 1e6;
-	$mn = $inf;
-	$mx = -$inf;
-	for ($i = 0; $i < $n; $i++) {
-		$cur = (int) $a[$i];
-		$mn = min($mn, $cur);
-		$mx = max($mx, $cur);
-	}
-	echo $mn." ".$mx."\n";
+$n = (int) fgets(STDIN);
+$a = explode(' ', fgets(STDIN));
+$inf = (int) 1e6;
+$mn = $inf;
+$mx = -$inf;
+for ($i = 0; $i < $n; $i++) 
+{
+ $cur = (int) $a[$i];
+ $mn = min($mn, $cur);
+ $mx = max($mx, $cur);
+}
+echo $mn." ".$mx."\n";
 ?>
