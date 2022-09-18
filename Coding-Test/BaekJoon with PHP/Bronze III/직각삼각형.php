c6143e38ccee7c44f1f7ead7ad955f9b95c1a945
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-09-18 14:15:31 +0900

    Update 직각삼각형.php

--- Coding-Test/BaekJoon with PHP/Bronze III/직각삼각형.php
+++ Coding-Test/BaekJoon with PHP/Bronze III/직각삼각형.php
@@ -1,15 +1,18 @@
 <?php
-while (true) {
+while (true) 
+{
   fscanf (STDIN,"%d %d %d",$a,$b,$c);
   $num_list = array($a, $b, $c);
   sort($num_list);
 
-  if ($a==0 && $b==0 && $c==0) {
-    break;
-  } else {
-    if (($num_list[0]*$num_list[0]) + ($num_list[1]*$num_list[1]) == ($num_list[2]*$num_list[2])) {
+  if ($a==0 && $b==0 && $c==0) {break;} 
+  else 
+  {
+    if (($num_list[0]*$num_list[0]) + ($num_list[1]*$num_list[1]) == ($num_list[2]*$num_list[2])) 
+    {
       fprintf(STDOUT,"%s\n",'right');
-    } else {
+    } else 
+    {
       fprintf(STDOUT,"%s\n",'wrong');
     }
   }
