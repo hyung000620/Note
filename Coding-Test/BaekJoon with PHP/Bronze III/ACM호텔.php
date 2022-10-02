173e78e8e903a696775ae1a799a11da60ba94a44
JunHyung <81272766+hyung000620@users.noreply.github.com>
2022-10-02 12:15:39 +0900

    Update ACM호텔.php

--- Coding-Test/BaekJoon with PHP/Bronze III/ACM호텔.php
+++ Coding-Test/BaekJoon with PHP/Bronze III/ACM호텔.php
@@ -1,6 +1,7 @@
 <?php
 $tt = intval(fgets(STDIN));
-for($t=0; $t<$tt; $t++) {
+for($t=0; $t<$tt; $t++) 
+{
     fscanf(STDIN, '%d %d %d', $h, $w, $n);
     echo ($n-1)%$h*100 + intdiv($n-1,$h) + 101 . "\n";
 }
